e36a7f87cb1e52c07c1f75ce42a3ebea85b22132
LaisGSenna <61289963+LaisGSenna@users.noreply.github.com>
2020-03-03 20:48:13 -0300

    Add files via upload

---
+++ Elevador.java
@@ -0,0 +1,25 @@
+package br.lasalle.oop.trab1;
+
+public class Elevador {
+	String modelo;
+	String marca;
+	int ano;
+	float capacidade; 
+	boolean aberto;
+	
+	void funcionar() {
+		if(!aberto) 
+			System.out.println("Em funcionamento!");
+		else
+		    System.out.println("Portas abertas! Não está operando!");
+		
+	}
+	
+	void abrir() {
+		aberto = true;
+	}
+	
+	void fechar() {
+		aberto = false;
+	}
+}
---
+++ PortaTester.java
@@ -0,0 +1,16 @@
+package br.lasalle.oop.trab1;
+
+public class PortaTester {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Carro carroPreto = new Carro();
+		carroPreto.cor = "preto";
+		
+		carroPreto.fechar();
+		
+		carroPreto.funcionar();
+		
+	}
+
+}
